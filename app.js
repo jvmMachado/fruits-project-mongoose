a07bf101e65f9f404f50010f94fcc81ba16ff6e7
jvmMachado <joaovmmachado@gmail.com>
2020-02-17 10:22:25 -0300

    add functions to update and delete documents in mongodb

--- app.js
+++ app.js
@@ -1,66 +1,85 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/fruitsDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/fruitsDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
-const fruitSchema = new mongoose.Schema ({
-    name: {
-        type: String,
-        required: [true, 'Please, enter a name.']
-    },
-    rating: {
-        type: Number,
-        min: 1,
-        max: 10,
-        validate: {
-            validator: Number.isInteger,
-            message: '{VALUE} is not an integer value'
-        }
-    },
-    review: String  
+const fruitSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please, enter a name.']
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 10,
+    validate: {
+      validator: Number.isInteger,
+      message: '{VALUE} is not an integer value'
+    }
+  },
+  review: String
 });
 
 const Fruit = mongoose.model('Fruit', fruitSchema);
 
-const apple = new Fruit ({
-    name: 'Apple',
-    rating: 7,
-    review: 'Pretty solid as a fruit.'
+const apple = new Fruit({
+  name: 'Apple',
+  rating: 7,
+  review: 'Pretty solid as a fruit.'
 });
 
-const kiwi = new Fruit ({
-    name: 'Kiwi',
-    rating: 4,
-    review: `I don't get it.`
+const kiwi = new Fruit({
+  name: 'Kiwi',
+  rating: 4,
+  review: `I don't get it.`
 });
 
-const orange = new Fruit ({
-    name: 'Orange',
-    rating: 8,
-    review: `Makes for a great juice. Kinda sour though.`
+const orange = new Fruit({
+  name: 'Orange',
+  rating: 8,
+  review: `Makes for a great juice. Kinda sour though.`
 });
 
-const banana = new Fruit ({
-    name: 'Banana',
-    rating: 10,
-    review: `The best fruit out there!.`
+const banana = new Fruit({
+  name: 'Banana',
+  rating: 10,
+  review: `The best fruit out there!.`
 });
 
-Fruit.insertMany([apple, kiwi, orange, banana], function(err){
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('Successfully saved all the fruits to fruitsDB');
-    }
+Fruit.insertMany([apple, kiwi, orange, banana], function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Successfully saved all the fruits to fruitsDB');
+  }
 });
 
-Fruit.find(function(err, fruits){
-    if (err) {
-        console.log(err)
-    } else {
-        fruits.forEach(fruit => {
-            console.log(fruit.name);
-        });
+Fruit.updateOne({ name: 'Apple' }, { name: 'Peach' }, err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Successfully updated the document.');
+  }
+});
 
-        mongoose.connection.close();
-    }
+Fruit.deleteOne({ name: 'Orange' }, err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Successfully deleted the document.');
+  }
+});
+
+Fruit.find(function(err, fruits) {
+  if (err) {
+    console.log(err);
+  } else {
+    fruits.forEach(fruit => {
+      console.log(fruit.name);
+    });
+
+    mongoose.connection.close();
+  }
 });
